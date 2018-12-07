862fc278476d01693888a75091e47fdd2d1aca1d
riaz4519 <riaz.i3216@gmail.com>
2018-12-07 23:55:38 +0600

    after 18

---
+++ vueninjs2/cli_project/vuecli1/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <h1>{{ title }}</h1>
+    <p>{{ greetings()}}</p>
+
+  </div>
+</template>
+
+<script>
+
+
+export default {
+
+    data(){
+      return {
+          title:'welcome to your vuejs.app'
+      }
+    },
+    methods:{
+
+        greetings:function () {
+
+
+            return 'Hello world'
+
+
+        }
+    }
+
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
