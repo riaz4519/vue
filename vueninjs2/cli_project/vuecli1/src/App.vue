a1413d9bb183054af5bfd07e82872fe0bf90636a
riaz4519 <riaz.i3216@gmail.com>
2018-12-09 23:39:00 +0600

    after 22

--- vueninjs2/cli_project/vuecli1/src/App.vue
+++ vueninjs2/cli_project/vuecli1/src/App.vue
@@ -4,7 +4,7 @@
 
     <app-header></app-header>
 
-    <app-ninjas></app-ninjas>
+    <app-ninjas v-bind:ninjas="ninjas"></app-ninjas>
 
     <app-footer></app-footer>
 
@@ -30,6 +30,15 @@ export default {
     data(){
 
         return {
+
+            ninjas: [
+                {name: 'Ryu', speciality: 'Vue Components', show: false},
+                {name: 'Crystal', speciality: 'HTML Wizardry', show: false},
+                {name: 'Hitoshi', speciality: 'Click Events', show: false},
+                {name: 'Tango', speciality: 'Conditionals', show: false},
+                {name: 'Kami', speciality: 'Webpack', show: false},
+                {name: 'Yoshi', speciality: 'Data Diggin', show: false}
+            ],
 
         }
     }
