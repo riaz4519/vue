84f0be100db5c9dacde7f11619decee642119f27
unknown <riaz.i3216@gmail.com>
2018-12-24 19:49:56 +0600

    dynamic component running

--- vueninjs2/cli_project/vuecli27/src/App.vue
+++ vueninjs2/cli_project/vuecli27/src/App.vue
@@ -1,37 +1,22 @@
 <template>
   <div id="app">
 
-    <form-helper>
 
-
-      <div slot="form-header">
-
-        <h3>This is the title of the form</h3>
-        <p>Information about the form</p>
-
-      </div>
-      <div slot="form-fields">
-        <input type="text" placeholder="name" required>
-        <input type="password" placeholder="password" required>
-
-      </div>
-      <div slot="form-controls">
-
-        <button @click="handleSubmit">Submit</button>
-
-      </div>
-
-    </form-helper>
+    <form-one></form-one>
+    <form-two></form-two>
 
   </div>
 </template>
 
 <script>
 
-  import formHelper from './components/formHelper.vue'
+ import formOne from './components/formOne.vue'
+ import formTwo from './components/formTwo.vue'
 
 
 export default {
+
+
 
   data(){
 
@@ -42,7 +27,11 @@ export default {
   },
   name: 'app',
   components: {
-    'form-helper':formHelper,
+
+      'form-one':formOne,
+      'form-two':formTwo
+
+
   }
 }
 </script>
