b41f86ea21056ed4a0af182f72a4141026a75216
unknown <riaz.i3216@gmail.com>
2019-01-06 11:45:23 +0600

    after 34 custom directives

--- vueninjs2/cli_project/ninja29/src/main.js
+++ vueninjs2/cli_project/ninja29/src/main.js
@@ -7,6 +7,41 @@ import VueResource from 'vue-resource'
 
 Vue.use(VueResource);
 
+//custom directives
+
+Vue.directive('rainbow',{
+
+  bind(el,binding,vnode){
+
+    el.style.color = "#"+Math.random().toString().slice(2,8);
+
+  }
+
+});
+
+Vue.directive('theme',{
+
+  bind(el,binding,vnode){
+
+    if (binding.value == "wide"){
+
+      el.style.maxWidth = "1200px";
+
+    }
+    else if (binding.value == "narrow"){
+
+      el.style.maxWidth = "550px;"
+    }
+
+    if (binding.arg == 'column'){
+
+      el.style.background = '#ddd';
+      el.style.padding = '20px';
+    }
+
+  }
+});
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
