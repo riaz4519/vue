60c89a22ff90e4b4d6be3a9712db9ac7f7efbc62
unknown <riaz.i3216@gmail.com>
2019-01-14 18:01:33 +0600

    after adding vuex

---
+++ project/vuestate/src/store.js
@@ -0,0 +1,25 @@
+/**
+ * Created by User on 1/14/2019.
+ */
+
+import Vue from 'vue'
+
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default {
+
+    state:{
+
+        title:'My Custom Title',
+
+    },
+
+    mutations:{
+
+    },
+    actions:{
+
+    }
+}
