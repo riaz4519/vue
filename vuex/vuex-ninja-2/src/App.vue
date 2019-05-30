134c134c89844b39df0e5ad863258ffd2afaa6b4
unknown <riaz.i3216@gmail.com>
2019-05-30 15:38:28 +0600

    after vuex-2

---
+++ vuex/vuex-ninja-2/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+
+
+    <product-list-one v-bind:products="products"></product-list-one>
+    <product-list-two v-bind:products="products"></product-list-two>
+
+
+  </div>
+</template>
+
+<script>
+    import ProductListOne from './components/ProductListOne.vue'
+import ProductListTwo from './components/ProductListTwo.vue'
+
+export default {
+  name: 'app',
+  data(){
+
+    return {
+
+        products: [
+            {name: 'Banana Skin', price: 20},
+            {name: 'Shiny Star', price: 40},
+            {name: 'Green Shells', price: 60},
+            {name: 'Red Shells', price: 80}
+        ]
+    }
+  },
+  components: {
+
+      'product-list-one':ProductListOne,
+      'product-list-two':ProductListTwo,
+
+
+  }
+}
+</script>
+
+<style>
+  
+  body{
+    font-family: Ubuntu;
+    color: #555;
+  }
+
+</style>
