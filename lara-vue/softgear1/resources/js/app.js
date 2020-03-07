9e32507a9a17ebd087bbbc7845be30e8bce9474e
riaz4519 <riaz.i3216@gmail.com>
2020-03-07 22:25:17 +0600

    mixed

---
+++ lara-vue/softgear1/resources/js/app.js
@@ -0,0 +1,10 @@
+import Vue from 'vue';
+
+import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap'
+
+const app = new Vue({
+    el: '#app',
+    render: h => h(App)
+});
